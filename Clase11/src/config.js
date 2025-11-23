bdeb4f75c3dcb56e78f2e6be91bd273867a10b8f
arqluk <alejandro.lukacher@gmail.com>
2025-11-23 19:38:16 -0300

    actualización clase11

---
+++ Clase11/src/config.js
@@ -0,0 +1,13 @@
+// info sensible que no debería subirse al repositorio, 
+// debería añadirse este archivo al .gitignore
+const PORT = 8080
+const STRC = "mongodb://127.0.0.1"
+const NAMEBASE = "DBPrueba"                                   // "DBPruebaBis"
+const PERSISTENCE = "MONGO"
+
+export default {
+    PORT,
+    STRC,
+    NAMEBASE,
+    PERSISTENCE
+}
